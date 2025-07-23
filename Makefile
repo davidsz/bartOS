8e68fe2143a337955243af1550bc7197e301d53b
Szabolcs David <davidsz@inf.u-szeged.hu>
2025-07-24 00:53:27 +0200

    Bootloader: load the raw kernel binary from disk

--- Makefile
+++ Makefile
@@ -1,26 +1,28 @@
 SRCDIR   := src
 BUILDDIR := build
-ISO      := $(BUILDDIR)/bartos.iso
+# CD-ROM with GRUB included
+CD_IMG   := $(BUILDDIR)/bartos.iso
 BOOTSRC  := bootloader
-RAWIMG   := $(BUILDDIR)/bartos.bin
+# Hard disk image with the custom bootloader
+HD_IMG   := $(BUILDDIR)/bartos.bin
 
-CC       := i686-elf-g++
-CFLAGS   := -c -ffreestanding -Wall -Wextra -fno-exceptions -fno-rtti
-LD       := i686-elf-g++
-LDFLAGS  := -T link.ld -ffreestanding -nostdlib -lgcc
 AS       := nasm
-ASFLAGS  := -f elf32 -g
+ASFLAGS  := -g -f elf32
+CC       := i686-elf-g++
+CFLAGS   := -g -O0 -ffreestanding -fno-builtin -Wall -Wextra -Werror -nostdlib -nostartfiles -nodefaultlibs
+LD       := i686-elf-ld
+LDFLAGS  := -T link.ld
 
 ASSES    := $(wildcard $(SRCDIR)/*.s) $(wildcard $(SRCDIR)/**/*.s)
 SOURCES  := $(wildcard $(SRCDIR)/*.cpp) $(wildcard $(SRCDIR)/**/*.cpp)
 OBJECTS  := $(patsubst $(SRCDIR)/%.s,$(BUILDDIR)/%_s.o,$(ASSES)) \
 			$(patsubst $(SRCDIR)/%.cpp,$(BUILDDIR)/%_cpp.o,$(SOURCES))
 
-all: $(ISO) $(RAWIMG)
-boot-grub: $(ISO)
-boot-custom: $(RAWIMG)
+all: $(CD_IMG) $(HD_IMG)
+boot-grub: $(CD_IMG)
+boot-custom: $(HD_IMG)
 
-$(ISO): kernel.elf
+$(CD_IMG): kernel.elf
 	cp -R iso $(BUILDDIR)/iso
 	cp $(BUILDDIR)/kernel.elf $(BUILDDIR)/iso/boot/kernel.elf
 	mkisofs -R                                                  \
@@ -28,23 +30,28 @@ $(ISO): kernel.elf
 		-no-emul-boot                                           \
 		-boot-load-size 4                                       \
 		-boot-info-table                                        \
-		-o $(ISO)                                               \
+		-o $(CD_IMG)                                               \
 		$(BUILDDIR)/iso
-
-$(RAWIMG): kernel.elf bootloader.bin
-	dd if=$(BUILDDIR)/bootloader.bin >> $(RAWIMG)
-	dd if=$(BUILDDIR)/kernel.elf >> $(RAWIMG)
-	dd if=/dev/zero bs=1048576 count=16 >> $(RAWIMG)
-
-bootloader.bin: $(BOOTSRC)/bootloader.s
-	$(AS) -f bin $(BOOTSRC)/bootloader.s -o $(BUILDDIR)/bootloader.bin
 
 kernel.elf: $(OBJECTS)
 	$(LD) $(LDFLAGS) $(OBJECTS) -o $(BUILDDIR)/kernel.elf
 
+$(HD_IMG): kernel.bin bootloader.bin
+	rm -rf $(HD_IMG)
+	dd if=$(BUILDDIR)/bootloader.bin >> $(HD_IMG)
+	dd if=$(BUILDDIR)/kernel.bin >> $(HD_IMG)
+	dd if=/dev/zero bs=512 count=100 >> $(HD_IMG)
+
+bootloader.bin: $(BOOTSRC)/bootloader.s
+	$(AS) -f bin $(BOOTSRC)/bootloader.s -o $(BUILDDIR)/bootloader.bin
+
+kernel.bin: $(OBJECTS)
+	$(LD) -g -relocatable $(OBJECTS) -o $(BUILDDIR)/kernelfull.o
+	$(CC) -g -T link_bin.ld $(CFLAGS) $(BUILDDIR)/kernelfull.o -o $(BUILDDIR)/kernel.bin
+
 $(BUILDDIR)/%_cpp.o: $(SRCDIR)/%.cpp
 	mkdir -p $(@D)
-	$(CC) $(CFLAGS) $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILDDIR)/%_s.o: $(SRCDIR)/%.s
 	mkdir -p $(@D)
--- bootloader/bootloader.s
+++ bootloader/bootloader.s
@@ -8,11 +8,11 @@ DATA_SEG equ gdt_data - gdt_start
 
 ; Reserve the first 36 bytes for the BIOS Parameter Block
 ; The first 3 bytes of this is reserved for jumpig over the non-executeable data
-jmp short start  ; 2 bytes
+jmp short step1  ; 2 bytes
 nop              ; 1 byte
 times 33 db 0    ; 33 bytes
 
-start:
+step1:
     ; Start with setting a default value to all segment registers to maintain full control
     jmp 0:step2  ; Perform a far jump to set the CS register
 step2:
@@ -41,7 +41,7 @@ step2:
     mov cr0, eax
 
     ; Set the value of the Code Segment register to CODE_SEG
-    ; (the offset of the code segment descriptor) by jumpig
+    ; (the offset of the code segment descriptor) by far jumpig
     jmp CODE_SEG:start_protected
 
 ; Global Descriptor Table
@@ -51,6 +51,7 @@ gdt_null:
     dd 0x0
     dd 0x0
 
+; The base 0 and a limit 0xFFFFFFFF is stated in the multiboot specs
 gdt_code:         ; offset 0x8; CS should point to this
     dw 0xffff     ; Segment limit first 0-15 bits
     dw 0          ; Base first 0-15 bits
@@ -59,6 +60,7 @@ gdt_code:         ; offset 0x8; CS should point to this
     db 11001111b  ; High 4 bit flags and the low 4 bit flags
     db 0          ; Base 24-31 bits
 
+; The base 0 and a limit 0xFFFFFFFF is stated in the multiboot specs
 gdt_data:         ; offset 0x10; DS, SS, ES, FS, GS
     dw 0xffff     ; Segment limit first 0-15 bits
     dw 0          ; Base first 0-15 bits
@@ -66,8 +68,8 @@ gdt_data:         ; offset 0x10; DS, SS, ES, FS, GS
     db 10010010b  ; Access byte
     db 11001111b  ; High 4 bit flags and the low 4 bit flags
     db 0          ; Base 24-31 bits
-
 gdt_end:
+
 ; A struct that describes the place of GDT in memory
 gdt_addr:
     dw gdt_end - gdt_start-1 ; size
@@ -87,9 +89,78 @@ start_protected:
     ; Enable the A20 line (to use more then 20 memory buses and access more then 1MB of memory)
     in al, 0x92       ; Read the value of System Control Port A
     or al, 00000010b  ; Set the corresponding bit to 1
-    out 0x92, al      ; Write back the value of the port
+    out 0x92, al      ; Write back the value to the port
 
-    ; TODO: Load the kernel here
+    ; Load the kernel here from disk to memory
+    mov eax, 1               ; LBA (Logical Block Address); we start from the first sector
+    mov ecx, 100             ; Number of sectors to read
+    mov edi, 0x0100000       ; Destination address (at 1Mb)
+    call ata_lba_read        ; Call the routine
+    jmp CODE_SEG:0x0100000   ; Jump to the loaded kernel
+
+    ; TODO: This works for a raw binary kernel.
+    ; We should implement ELF support here.
+
+; Using the ATA controller to read sectors from the hard drive, addressed by Logical Block Address
+ata_lba_read:
+    mov ebx, eax ; Backup the LBA and restore it later in each step
+
+    ; Device/Head register (port 0x1F6)
+    ; 7   6   5   4   3   2   1   0
+    ; 1   L   1   D   H   H   H   H
+    ; -----------------------------
+    ; 1 = Reserved bits
+    ; L = LBA mode enabled
+    ; D = Device (master is 0, slave is 1)
+    ; H = highest 4 bits of the LBA
+    ; -----------------------------
+    shr eax, 24        ; Shift the highest 4 bits of the LBA into the lower 4 bits
+    or eax, 11100000b  ; Select LBA mode and the master drive
+    mov dx, 0x1F6      ; Specify the port for the Device/Head register
+    out dx, al         ; Send the value to the port (AL is the lower 8 bits of EAX)
+
+    ; Sector Count register (port 0x1F2)
+    mov eax, ecx       ; We specified the number of sectors to read earlier in ECX
+    mov dx, 0x1F2
+    out dx, al
+
+    ; LBA Low, bits 0-7 (port 0x1F3)
+    mov eax, ebx       ; Restore the backup LBA
+    mov dx, 0x1F3
+    out dx, al         ; It's easy to send only the lowest 8 bits of EAX
+
+    ; LBA Mid, bits 8-15 (port 0x1F4)
+    mov eax, ebx       ; Restore the backup LBA
+    mov dx, 0x1F4
+    shr eax, 8         ; Bring the next 8 bits of the LBA into the lower 8 bits of EAX
+    out dx, al
+
+    ; LBA High, bits 16-23 (port 0x1F5)
+    mov eax, ebx       ; Restore the backup LBA
+    mov dx, 0x1F5
+    shr eax, 16
+    out dx, al
+
+    ; Command/Status register (port 0x1F7)
+    mov dx, 0x1f7
+    mov al, 0x20       ; Reading (0x20) or writing (0x30) sectors
+    out dx, al
+
+.next_sector:            ; Reading a sector
+    push ecx             ; ECX is the loop counter, but will have an other purpose, so save it on stack
+.try_again:              ; Checking if we can read by polling in a loop
+    mov dx, 0x1f7        ; Reading status from the Command/Status register
+    in al, dx
+    test al, 00001000b   ; Test the Data Request Ready (DRQ) bit
+    jz .try_again
+
+    ; We can read a sector from the Data register (port 0x1F0)
+    mov ecx, 256         ; Number of words to read; 256 * 2 bytes = 512 bytes = 1 sector
+    mov dx, 0x1F0
+    rep insw             ; Read ECX number of words from the Data register into [ES:DI] and increases DI by 2
+    pop ecx              ; Restore ECX from stack
+    loop .next_sector    ; Decrease ECX and repeat
+    ret                  ; End of the ata_lba_read routine
 
 ; Pad the rest of the space with zeros
 times 510-($ - $$) db 0
