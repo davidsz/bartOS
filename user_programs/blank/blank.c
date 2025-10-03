16ad93c025ae77aac44a032305a7ea8dd3f5dc5d
Szabolcs David <davidsz@inf.u-szeged.hu>
2025-10-03 15:00:17 +0200

    Start creating a libc and compiling user programs from C language

---
+++ user_programs/blank/blank.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+
+int main(int, char **)
+{
+    print("WEEEEEE!\n");
+    return 0;
+}
---
+++ user_programs/libc/include/stdio.h
@@ -0,0 +1,6 @@
+#ifndef LIBC_STDIO_H
+#define LIBC_STDIO_H
+
+void print(const char *message);
+
+#endif // LIBC_STDIO_H
